dd02922d2fa0c4896e3c38748e654203c9af9b14
spartango@gmail.com <spartango@gmail.com>
2012-01-28 01:59:47 -0500

    more stubs and a rename

---
+++ pipeline/src/Pipeline.cpp
@@ -0,0 +1,8 @@
+int main() {
+    // This should create the ImageProcessor
+    // Fetch it some labeled Images
+    // Run the processor
+    // Score the output
+    printf("TODO: implement pipeline around Image Processors\n");
+
+}
--- pipeline/src/test/testProcessor.cpp
+++ pipeline/src/test/testProcessor.cpp
@@ -1,4 +1,3 @@
-
 int main() {
     printf("TODO: implement basic tests of Image Processors\n");
 }
