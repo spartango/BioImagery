53eaf986d36c124fc726f12c92933d65361ce946
spartango@gmail.com <spartango@gmail.com>
2012-01-29 03:48:16 -0500

    laid out what a typical pipeline might look like

--- pipeline/src/Pipeline.cpp
+++ pipeline/src/Pipeline.cpp
@@ -5,24 +5,34 @@
 #include "ImageProcessor.h"
 
 #define NUM_IMAGES 8
+#define LABEL_HOST "proto.melamp.us"
 
 using namespace std;
 using namespace Bioimagery;
 
 int main() {
-    vector<LabeledImage> targetImages;
+    vector<LabeledImage*> targetImages;
 
     // Fetch it some labeled Images
     for(int i = 0; i < NUM_IMAGES; i++) {
-        targetImages.push_back(LabeledImage(i));
+        LabeledImage *newImage = new LabeledImage(i);
+        newImage->load(LABEL_HOST);
+        targetImages.push_back(newImage);
     }
 
     // This should create the ImageProcessor
+    ImageProcessor processor(targetImages, 0);
 
     // Run the processor
+    processor.processImages();
 
     // Score the output
+    printf("TODO: implement scoring around Image Processors\n");
 
-    printf("TODO: implement pipeline around Image Processors\n");
+    // Clean up Images
+    for(int i = 0; i < targetImages.size(); i++) {
+        delete targetImages[i];
+    }
+
 
 }
