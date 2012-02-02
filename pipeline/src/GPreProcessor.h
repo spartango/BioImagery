545b9b38e7477b7a94c54990928c6de710e36ad4
spartango@gmail.com <spartango@gmail.com>
2012-02-02 10:37:24 -0500

    added a gaussian filter

---
+++ pipeline/src/GPreProcessor.h
@@ -0,0 +1,44 @@
+#ifndef GPREPROCESSOR_H_
+#define GPREPROCESSOR_H_
+
+#include <LabeledImage.h>
+#include "ImageProcessor.h"
+#include <vector>
+
+using namespace std;
+
+namespace Bioimagery {
+
+    class GPreProcessor : public ImageProcessor {
+
+    public:
+        GPreProcessor(vector<LabeledImage*> images, 
+                      int targetIndex, 
+                      int filter_width, 
+                      int filter_height, 
+                      double sigma_x,
+                      double sigma_y);
+
+        GPreProcessor(LabeledImage **images, 
+                      int numImages, 
+                      int targetIndex,
+                      int filter_width, 
+                      int filter_height, 
+                      double sigma_x,
+                      double sigma_y);
+
+        // Runs image processing and returns the target image
+        virtual LabeledImage* processImages();
+    
+    private:
+        int filter_width;
+        int filter_height;
+        double sigma_x;
+        double sigma_y;
+
+    };
+
+}
+
+#endif /* GPREPROCESSOR_H_ */
+
